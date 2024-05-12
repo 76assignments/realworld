c08bea6761b16bb75a6d8cd2b78fa37b444ade52
76assignments <76assignments@gmail.com>
2024-05-12 16:23:09 +0530

    update script

--- scripts/install_gunicorn.sh
+++ scripts/install_gunicorn.sh
@@ -4,6 +4,7 @@
 PROJECT_MAIN_DIR_NAME="conduit"
 current_user=$(whoami)
 sed -i "s/ubuntu/$current_user/g" "/home/$current_user/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.service"
+sed -i "s/www-data/$current_user/g" "/home/$current_user/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.service"
 # Copy gunicorn  service file
 sudo cp "/home/$current_user/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.service" "/etc/systemd/system/gunicorn.service"
 
